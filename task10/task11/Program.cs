88259be2bfec22a98d1dcbc9a3c521df4215f2cb
Sergio <sergio977@rambler.ru>
2022-11-22 16:43:32 +0500

    add task11 new

--- task10/task11/Program.cs
+++ task10/task11/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая выводит случайное трехзначное число
+и удаляет вторую цифру этого числа
+Например:
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+int GetRandomNumberInRange(int start, int end)
+{
+    int result = 0;
+    result = new Random().Next(start, end + 1);
+    return result;
+}
+int GetSecondNumber(int number)
+{
+    int firstDigit = number / 100;
+    int secondDigit = firstDigit % 10;
+    int threeDigit = number % 10;
+    int NewNumber = firstDigit * 10 + threeDigit;
+    return NewNumber;
+
+}
+int randomNumber = GetRandomNumberInRange(100, 999);
+Console.WriteLine(randomNumber);
+int newNumber = GetSecondNumber(randomNumber);
+Console.WriteLine($" {newNumber} ");
